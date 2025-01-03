230d4f91fedbc0ab037d4fa51a55e2a14a01d2d8
Arnav Lahane <119531646+Arrnv@users.noreply.github.com>
2025-01-03 15:00:47 +0530

    ETL Pipeline

--- NetworkSecurity/exception/exception.py
+++ NetworkSecurity/exception/exception.py
@@ -1,19 +1,14 @@
 import sys
+from NetworkSecurity.logging import logger
 
-def error_message_detail(error,error_detail:sys):
-    _,_,exc_tb=error_detail.exc_info()
-    file_name=exc_tb.tb_frame.f_code.co_filename
-    error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
-     file_name,exc_tb.tb_lineno,str(error)
-    )
-
-    return error_message
-
-
-class CustomException(Exception):
+class NetworkSecurityException(Exception):
     def __init__(self,error_message,error_details:sys):
-        super().__init__(error_message)
-        self.error_message=error_message_detail(error_message,error_details)
-
+        self.error_message = error_message
+        _,_,exc_tb = error_details.exc_info()
+        
+        self.lineno=exc_tb.tb_lineno
+        self.file_name=exc_tb.tb_frame.f_code.co_filename 
+    
     def __str__(self):
-        return self.error_message
+        return "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
+        self.file_name, self.lineno, str(self.error_message))
